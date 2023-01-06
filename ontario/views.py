8a0973a51e561ce369e075e29f898b9d70fe3f6c
huutrungrimp <huutrungrimp@gmail.com>
2023-01-06 17:40:14 -0500

    remove age

--- ontario/views.py
+++ ontario/views.py
@@ -8,9 +8,11 @@ import datetime
 import pandas as pd
 from urllib.error import HTTPError
 from .getData import getOnEpi, getDemographyData
+# import numpy
+from memory_profiler import profile
 
 
-
+@profile
 def epiGraph_ON(request):
     epi_phu = getOnEpi()
     active = epi_phu.groupby(['reported_date']).sum()
@@ -19,6 +21,10 @@ def epiGraph_ON(request):
 
     return JsonResponse(ontario['data'], safe=False)
 
+@profile
 def epiDemography_ON(request):
     demographyData = getDemographyData()
     return JsonResponse(demographyData, safe=False)
+
+if __name__ == '__main__':
+    epiGraph_ON
