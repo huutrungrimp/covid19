c8ceb4b75270d3c0f86f8c1c79edd74599867423
huutrungrimp <huutrungrimp@gmail.com>
2023-01-07 06:26:18 -0500

    dtype count

--- ontario/getData.py
+++ ontario/getData.py
@@ -18,16 +18,28 @@ def getOnEpi():
     col = col.str.replace(',', '')
     col = col.str.replace('.', '', regex=True)
     newData.columns = col
+
     return newData          
 
 @profile
 def getDemographyData():
     conf_phu = pd.read_csv("https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv", 
-    usecols = ['Case_Reported_Date', 'Client_Gender'], 
+    usecols = ['Case_Reported_Date', 'Age_Group', 'Client_Gender'], 
     low_memory = True,
-    dtype={'Case_Reported_Date':'string', 'Client_Gender':'category'}
+    dtype={'Case_Reported_Date':'string', 'Age_Group':'category', 'Client_Gender':'category'}
     )
-    conf_phu['count']=1
+    conf_phu['count']=numpy.ones((len(conf_phu['Age_Group']), 1), dtype='int16')
+    # conf_phu['count'] = 1
+
+    age = conf_phu[['Case_Reported_Date', 'Age_Group']]
+    age=conf_phu.groupby(['Case_Reported_Date', 'Age_Group']).count().reset_index()
+    age=age.pivot(index='Case_Reported_Date', columns='Age_Group', values='count')
+    age = age.drop('UNKNOWN', axis=1)
+    
+    result = age.to_json(orient="table")
+    parsed = json.loads(result)
+    age_json = parsed['data']
+    age_json
 
     gender = conf_phu[['Case_Reported_Date', 'Client_Gender', 'count']]
     gender=gender.groupby(['Case_Reported_Date', 'Client_Gender']).count().reset_index()
@@ -40,7 +52,7 @@ def getDemographyData():
     gender_json
 
     demography = {
-        'age': 'age_json',
+        'age': age_json,
         'gender':gender_json
     }
     return demography
